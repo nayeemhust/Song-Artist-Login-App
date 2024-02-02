5bf2c3d808f4f4ae2dd002b1aa4fd9f23f92d9a6
Nazmul Alam Nayeem <nayeemhust@gmail.com>
2024-02-02 03:49:54 -0500

    modified Service Worker file

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,20 @@
 console.log(self);
 self.addEventListener('install', (event) => {
     console.log('[SW] Install:', event);
+// Activate itself, no need to wait for the user to activate
+    self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+    console.log('[SW] Activate:', event);
 
+// Immediately gets control over the client's page
+    event.waitUntil(clients.claim());
+
+    // This line will only be executed after 
+    // getting control over all open pages.
+    
+})
 
 
 self.addEventListener('fetch', () => {
