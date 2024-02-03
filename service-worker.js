1cb9c93a8ab2809f8cefb797586e5574743da214
Nazmul Alam Nayeem <nayeemhust@gmail.com>
2024-02-02 19:15:28 -0500

    service worker activation added

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,24 @@ self.addEventListener('install', (event) => {
     console.log('[SW] Install:', event);
 // Activate itself, no need to wait for the user to activate
     self.skipWaiting();
+
+
+console.log(caches);
+event.waitUntil(
+    caches.open('cacheAssets')
+.then((cache) => {
+    console.log('Cache', cache)
+    // cache.add('/index.html');
+    // cache.add('/app.js');
+    // cache.addAll([
+    //     '/index.html',
+    //     '/app.js']);
+
+})
+.catch((error) => {
+    console.log('Cache failed', error);
+})
+);
 });
 
 self.addEventListener('activate', (event) => {
@@ -13,10 +31,19 @@ self.addEventListener('activate', (event) => {
 
     // This line will only be executed after 
     // getting control over all open pages.
-    
+
 })
 
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+    caches.match(event.request)
+    .then((response) => {
+        console.log('Response:', response);
+        return response;
+    })
+    .catch((error) => {
+    console.log('Match Failed:', error);
+    })
+);
 
-self.addEventListener('fetch', () => {
-return;
 });
